1ff58a67a20183d0959a174cc1f07f0a1a509966
Jonas Furrer <jonas.furrer@gmail.com>
2023-01-17 10:34:35 +0100

    Improve popup
    - Remove (broken) click to copy functionality from duplicates
    - List duplicates instead of icon
    - Hide all (non-working) tooltips
    - Show parent
    - Improve close behaviour

--- cloudapp/src/app/result-table/result-table.component.ts
+++ cloudapp/src/app/result-table/result-table.component.ts
@@ -2,29 +2,32 @@ import { Component, Input, ViewChild } from '@angular/core'
 import { MatSort } from '@angular/material/sort'
 import { MatTableDataSource } from '@angular/material/table'
 import { CloudAppEventsService } from '@exlibris/exl-cloudapp-angular-lib'
+import { BibEntity } from '../models/bib-entity.model'
 import { BibInfo } from '../models/bib-info.model'
 
 @Component({
 	selector: 'app-result-table',
 	templateUrl: './result-table.component.html',
-	styleUrls: ['./result-table.component.scss']
+	styleUrls: ['./result-table.component.scss'],
 })
 export class ResultTableComponent {
 	displayedColumns: string[] = ['order', 'title', 'year', 'edition', 'mmsId', 'duplicate', 'analytical', 'holdings'];
 	instCode: string
-
+	inPopup: boolean
 	@Input()
 	result: MatTableDataSource<BibInfo>
+	@Input()
+	selectedEntity: BibEntity
 
 	@ViewChild(MatSort) sort: MatSort
 
 	constructor(
-		private events: CloudAppEventsService
+		private events: CloudAppEventsService,
 	) {
 		this.events.getInitData().subscribe(data => this.instCode = data.instCode)
 	}
 
-	getMatSort() {
+	getMatSort(): MatSort {
 		return this.sort
 	}
 
